6f7a89241fe896e161ee44e0ac28800a09559da2
Maksym Norokha <maxnorohich@gmail.com>
2024-03-13 18:35:06 +0100

    123

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,20 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        arrayDeque.add(firstQueue.remove());
+        arrayDeque.add(firstQueue.remove());
+        arrayDeque.add(secondQueue.remove());
+        arrayDeque.add(secondQueue.remove());
+        while(!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            firstQueue.add(arrayDeque.removeLast());
+            arrayDeque.addLast(firstQueue.remove());
+            arrayDeque.addLast(firstQueue.remove());
+
+            secondQueue.add(arrayDeque.removeLast());
+            arrayDeque.addLast(secondQueue.remove());
+            arrayDeque.addLast(secondQueue.remove());
+        }
+        return arrayDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,28 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i=1; i<=numberOfDishes; i++) {
+            queue.offer(i);
+        }
+        int size= queue.size();
+        List<Integer> result = new ArrayList<>();
+        while(result.size() <size) {
+            Integer element=0;
+            for (int i=0; i<everyDishNumberToEat; i++) {
+                element = queue.poll();
+                if (i != everyDishNumberToEat-1) {
+                    queue.offer(element);
+                }
+            }
+            result.add(element);
+        }
+        return result;
     }
 }
